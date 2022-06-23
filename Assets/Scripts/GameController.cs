811e790b990231d992a2526bbe138d9a8ed802e5
Maxim Kotlyarenko <s1mple.eq.easy@gmail.com>
2022-06-23 21:49:14 +0300

    commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] cells;
+    public int crossesOrZeros = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
